ba6871b2d18a4156b5bbcca0a7aec97c37204d66
G386632023 <238663@st.takushoku-u.ac.jp>
2024-08-07 22:57:09 +0900

    プロジェクト完成

--- weather.js
+++ weather.js
@@ -1,52 +1,7 @@
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
-
 ////////// 課題3-2 ここからプログラムを書こう
+let n = 0;
 
+function print (data) {
 console.log(data.name); 
 console.log(data.weather[0].description); 
 console.log(data.main.temp_max);
@@ -56,6 +11,7 @@ console.log(data.wind.speed);
 console.log(data.wind.deg);
 console.log(data.coord.lon); 
 console.log(data.coord.lat);
+
 
 let div = document.querySelector('div#result'); 
 let p = document.createElement('p');
@@ -94,6 +50,7 @@ let p9 = document.createElement('p');
 p9.textContent = "緯度: "+data.coord.lat;
 p8.insertAdjacentElement('afterend', p9);
 
+}
 
 
 let b = document.querySelector('button#print');
@@ -115,6 +72,11 @@ b.addEventListener('click', sendRequest);
 
   // 通信を開始する処理
 function sendRequest() {
+  n = n+1;
+  if (n > 1){
+    clear();
+  }
+
   let toshi = document.querySelector('select#toshi');
   let a = toshi.selectedIndex;
 
@@ -130,6 +92,7 @@ function sendRequest() {
       .catch(showError)   // 通信失敗
       .then(finish);      // 通信の最後の処理
 }
+
 
 // 通信が成功した時の処理
 function showResult(resp) {
@@ -159,3 +122,13 @@ function finish() {
   console.log('Ajax 通信が終わりました');
 }
 
+function clear() {
+  let div = document.querySelector('div#result');
+
+  let p = div.querySelectorAll('p');
+  for(let n of p){
+    n.remove();
+  }
+
+
+}
